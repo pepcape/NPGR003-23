b505212ec86b33a9a5f6f27261b9aff668eafdc1
Josef Pelikan <josef.pelikan@scssoft.com>
2024-01-05 18:23:40 +0100

    3D: VertexArrayObject Bind() fix.

--- Silk3D/shared/VertexArrayObject.cs
+++ Silk3D/shared/VertexArrayObject.cs
@@ -32,9 +32,9 @@ public class VertexArrayObject<TVertexType, TIndexType> : IDisposable
 
 	public void Bind()
 	{
+		_gl.BindVertexArray(_handle);
 		_vbo.Bind();
 		_ebo.Bind();
-		_gl.BindVertexArray(_handle);
 	}
 
 	public void Dispose()
@@ -46,38 +46,38 @@ public class VertexArrayObject<TVertexType, TIndexType> : IDisposable
 }
 
 public class VertexArrayObject<TVertexType> : IDisposable
-  where TVertexType : unmanaged
+	where TVertexType : unmanaged
 {
-  private uint _handle;
-  private GL _gl;
-  private BufferObject<TVertexType> _vbo;
+	private uint _handle;
+	private GL _gl;
+	private BufferObject<TVertexType> _vbo;
 
-  public VertexArrayObject (GL gl, BufferObject<TVertexType> vbo)
-  {
-    _gl = gl;
-    _vbo = vbo;
+	public VertexArrayObject (GL gl, BufferObject<TVertexType> vbo)
+	{
+		_gl = gl;
+		_vbo = vbo;
 
-    _handle = _gl.GenVertexArray();
-    Bind();
-  }
+		_handle = _gl.GenVertexArray();
+		Bind();
+	}
 
-  public unsafe void VertexAttributePointer (uint index, int count, VertexAttribPointerType type, uint vertexSize,
-    int offSet)
-  {
-    _gl.VertexAttribPointer(index, count, type, false, vertexSize * (uint)sizeof(TVertexType),
-      (void*)(offSet * sizeof(TVertexType)));
-    _gl.EnableVertexAttribArray(index);
-  }
+	public unsafe void VertexAttributePointer (uint index, int count, VertexAttribPointerType type, uint vertexSize,
+		int offSet)
+	{
+		_gl.VertexAttribPointer(index, count, type, false, vertexSize * (uint)sizeof(TVertexType),
+			(void*)(offSet * sizeof(TVertexType)));
+		_gl.EnableVertexAttribArray(index);
+	}
 
-  public void Bind ()
-  {
-    _vbo.Bind();
-    _gl.BindVertexArray(_handle);
-  }
+	public void Bind ()
+	{
+		_gl.BindVertexArray(_handle);
+		_vbo.Bind();
+	}
 
-  public void Dispose ()
-  {
-    _vbo.Dispose();
-    _gl.DeleteVertexArray(_handle);
-  }
+	public void Dispose ()
+	{
+		_vbo.Dispose();
+		_gl.DeleteVertexArray(_handle);
+	}
 }
